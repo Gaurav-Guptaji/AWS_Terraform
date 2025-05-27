0b2b32ce9950487476e5325591ba2484da4b83a9
Gaurav Gupta <lomagaurav@gmail.com>
2025-05-27 15:21:25 +0530

    Added subnet

--- main.tf
+++ main.tf
@@ -12,6 +12,13 @@ module "s3" {
   source      = "./modules/s3"
   bucket_name = "my-terraform-bucket-example"
 }
+module "subnet" {
+  source            = "./modules/subnet"
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  name              = "dev-subnet"
+}
 
 # Add EC2 after VPC and subnet exist
 module "ec2" {
